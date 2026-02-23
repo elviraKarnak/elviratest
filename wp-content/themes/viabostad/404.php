4e5fba57873d2f3a793160d18cf6469934766125
elviraKarnak <karnak@elvirainfotech.com>
2026-02-23 17:18:15 +0530

    file change

--- wp-content/themes/viabostad/404.php
+++ wp-content/themes/viabostad/404.php
@@ -7,7 +7,7 @@ get_header();
 <div class="container">
      <div class="textbox text-center" style="padding-bottom:50px;">
      <h1 class="common-title" data-aos="fade-right">404</h1>
-           <p>Page Not Found !</p>
+           <p>Page Not Founds !</p>
      </div>
 			
         </div>
